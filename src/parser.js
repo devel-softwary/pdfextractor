c5f76c163b5e638c7e07b8ef4eb0c6e32531fbee
GiuseppeLauro <giuseppe.lauro@openeconomics.eu>
2026-09-07 22:23:20 +0200

    Nuova UM: coppie

--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,7 @@
 import { segmentRow, repairIdentifierDescription, cleanText, vectorBoundariesNear } from './table-engine.js';
 const DATE_RE = /\b(\d{2}\/\d{2}\/\d{4})\b/;
 const NUM_RE = /-?\d[\d.'´’]*,\d+|-?\d[\d.'´’]*/g;
-const UM_RE = /^(cad|cadauno|cad\.|mq|mq\/cm|m2|m²|mc|m3|m³|kg|t|q|h|ora|gg|ml|m|cm|mm|ha|a corpo|corpo|%|l|lt|kW|W|V|A|nr|n\.|pz)$/i;
+const UM_RE = /^(coppie|cad|cadauno|cad\.|mq|mq\/cm|m2|m²|mc|m3|m³|kg|t|q|h|ora|gg|ml|m|cm|mm|ha|a corpo|corpo|%|l|lt|kW|W|V|A|nr|n\.|pz)$/i;
 
 function n(s) {
   if (s == null || s === '') return null;
